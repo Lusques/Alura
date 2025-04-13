be408e6b72f4155cf18f88447921958af8844a5c
Lucas Silva <lucascarlosjobs@gmail.com>
2025-04-13 13:44:14 -0300

    add validator to form fields

--- angular-components-cli/src/app/components/add-contact-form/add-contact-form.component.ts
+++ angular-components-cli/src/app/components/add-contact-form/add-contact-form.component.ts
@@ -1,10 +1,15 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-
+import { NgClass } from '@angular/common';
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-add-contact-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './add-contact-form.component.html',
   styleUrl: './add-contact-form.component.scss',
 })
@@ -13,12 +18,24 @@ export class AddContactFormComponent {
 
   constructor() {
     this.contactForm = new FormGroup({
-      name: new FormControl('Lucas Silva'),
-      telphone: new FormControl('99 99999-9999'),
-      email: new FormControl('lucas@teste.com.br'),
+      name: new FormControl('Lucas Silva', [Validators.required]),
+      telphone: new FormControl('99 99999-9999', [Validators.required]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+      ]),
       birth: new FormControl('2025-04-13'),
       url: new FormControl('https://github.com/Lusques'),
       observations: new FormControl('Hello World!'),
     });
+  }
+
+  createContact() {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+    }
+  }
+  submitCancel() {
+    console.log('submit cancelado');
   }
 }
