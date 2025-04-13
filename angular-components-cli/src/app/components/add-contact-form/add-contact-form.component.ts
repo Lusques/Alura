781e194760a754607e9132a28f5805c40536fc08
Lucas Silva <lucascarlosjobs@gmail.com>
2025-04-13 12:15:24 -0300

    uses contactGroup  module

--- angular-components-cli/src/app/components/add-contact-form/add-contact-form.component.ts
+++ angular-components-cli/src/app/components/add-contact-form/add-contact-form.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-contact-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-contact-form.component.html',
   styleUrl: './add-contact-form.component.scss',
 })
@@ -13,12 +13,12 @@ export class AddContactFormComponent {
 
   constructor() {
     this.contactForm = new FormGroup({
-      name: new FormControl('Nome'),
-      telphone: new FormControl(),
-      email: new FormControl(),
-      birth: new FormControl(),
-      url: new FormControl(),
-      observatios: new FormControl(),
+      name: new FormControl('Lucas Silva'),
+      telphone: new FormControl('99 99999-9999'),
+      email: new FormControl('lucas@teste.com.br'),
+      birth: new FormControl('2025-04-13'),
+      url: new FormControl('https://github.com/Lusques'),
+      observations: new FormControl('Hello World!'),
     });
   }
 }
