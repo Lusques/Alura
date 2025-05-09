5aafbcdeb4a1b888e29eeb94325ba4dc9a39815a
Lucas Silva <lucascarlosjobs@gmail.com>
2025-05-09 20:34:11 -0300

    feat(shared): add props to base button

--- crud/src/app/shared/components/buttons/base-button/base-button.component.ts
+++ crud/src/app/shared/components/buttons/base-button/base-button.component.ts
@@ -4,9 +4,13 @@ import { Component, Input, OnInit } from '@angular/core';
   selector: 'app-base-button',
   templateUrl: './base-button.component.html',
   styleUrls: ['./base-button.component.scss'],
+  host: {
+    '[class]': 'styleClass'
+  }
 })
 export class BaseButtonComponent implements OnInit {
   @Input() variant: 'primary' | 'neutral' = 'primary';
+  @Input() styleClass: string = '';
   constructor() {}
 
   ngOnInit(): void {}
