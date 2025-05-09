7b72ccd9767eedf570d2e441c484c3e08d306560
Lucas Silva <lucascarlosjobs@gmail.com>
2025-05-09 20:04:27 -0300

    feat(routes): creates routes

--- crud/src/app/app-routing.module.ts
+++ crud/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BaseLayoutComponent } from './features/layouts/base-layout/base-layout.component';
+import { NewQuotePageComponent } from './features/pages/dashboard/new-quote-page/new-quote-page.component';
+import { QuoteBoardPageComponent } from './features/pages/dashboard/quote-board-page/quote-board-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: BaseLayoutComponent,
+    children: [
+      { path: 'dashboard/new-quote', component: NewQuotePageComponent },
+      { path: 'dashboard/quote-board', component: QuoteBoardPageComponent },
+      { path: '**', redirectTo: 'dashboard/quote-board', pathMatch: 'full' },
+    ],
   },
 ];
 
