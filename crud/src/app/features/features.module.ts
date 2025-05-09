d6cac22bc064f7baa3fa7756e0f47066fb31755d
Lucas Silva <lucascarlosjobs@gmail.com>
2025-05-09 17:34:30 -0300

    feat(style): creates styles base

--- crud/src/app/features/features.module.ts
+++ crud/src/app/features/features.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [BaseLayoutComponent],
+  imports: [CommonModule],
+  exports: [BaseLayoutComponent],
 })
-export class FeaturesModule { }
+export class FeaturesModule {}
